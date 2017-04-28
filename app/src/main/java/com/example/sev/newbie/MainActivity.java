c69bdb29a755bf3974a9e65b5e92870cb129d49f
Jorge <jorge.alfmur@gmail.com>
2017-04-27 23:25:59 -0500

    implementamos boton simple usando INTERFACE

--- app/src/main/java/com/example/sev/newbie/MainActivity.java
+++ app/src/main/java/com/example/sev/newbie/MainActivity.java
@@ -5,16 +5,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView texto;
+    Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //capturamos elementos
+        texto = (TextView) findViewById(R.id.texto);
+        boton = (Button) findViewById(R.id.btn);
+        boton.setOnClickListener(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -48,5 +58,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        texto.setText("chau");
     }
 }
